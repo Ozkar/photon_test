3e2548b6edb9fe7eb7794d23ff34e08dee48407a
oleung <oleung@attinteractive.com>
2012-08-31 00:36:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotonFlickr::Application.config.secret_token = 'e343b2e0801066b1dba742332b343079b22bbd76b090e0e3b4d997788abab9197de2b729d31bf78681ba5346450ec8856a47e3e9e04eaca352cea65b331f82a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PhotonFlickr::Application.config.session_store :cookie_store, key: '_photon_flickr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PhotonFlickr::Application.config.session_store :active_record_store
